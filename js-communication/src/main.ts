a54761797aa27d7a48af47a49698caa5a1cd9d8f
mohammed-rharbi <rharbi383@gmail.com>
2024-11-01 15:43:29 +0100

    fix : fix database connection

--- js-communication/src/main.ts
+++ js-communication/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Listen for application events
+  app.enableShutdownHooks();
+
   await app.listen(process.env.PORT ?? 3000);
+  Logger.log(`Application is running on: http://localhost:${process.env.PORT ?? 3000}`, 'Bootstrap');
 }
 bootstrap();
