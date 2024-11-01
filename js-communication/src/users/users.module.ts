abb01c8dedacd32ce19465176470420be0bdbce5
mohammed-rharbi <rharbi383@gmail.com>
2024-11-01 20:16:58 +0100

    feat : register the conroller and service to the user module

--- js-communication/src/users/users.module.ts
+++ js-communication/src/users/users.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
-import { UsersService } from './users.service';
 import { User , userSchema } from './users.entity'
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserRepositorie } from './users.repositorie';
+import { UsersService } from './users.service';
 
 @Module({
   controllers: [UsersController],
