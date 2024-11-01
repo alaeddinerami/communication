5f15204e190e1a2352351b1b34c0899ddd73da3e
mohammed-rharbi <rharbi383@gmail.com>
2024-11-01 15:42:45 +0100

    perf : modified user service and controller and module

--- js-communication/src/users/users.service.ts
+++ js-communication/src/users/users.service.ts
@@ -1,30 +1,4 @@
-import { Prop , Schema , SchemaFactory } from "@nestjs/mongoose";
-import { Document } from "mongoose";
+import { Injectable } from '@nestjs/common';
 
-@Schema()
-export class User extends Document {
-
-    @Prop({ required: true})
-    userName: string;
-
-    @Prop({required: true , unique:true})
-    email: string
-    
-    @Prop({enum:['online','offline','banned'], default:'online'})
-    status : string
-
-    @Prop({enum:['user','moderator'] , default:'user'})
-    role : string
-
-    @Prop({type:[String] , default: [] })
-    friends : string[];
-
-    @Prop({type:[String] , default: [] })
-    channels: string[];
-
-    @Prop({type: Number , default: 0} )
-    score : number
-
-}
-
-export const userSchema = SchemaFactory.createForClass(User);
+@Injectable()
+export class UsersService {}
