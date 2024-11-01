ece6fdb5b2287433c4e9294852c642b4dbb5b475
mohammed-rharbi <rharbi383@gmail.com>
2024-11-01 09:14:42 +0100

    fix : fix Notifications components

--- frontend/src/components/notifications.tsx
+++ frontend/src/components/notifications.tsx
@@ -10,7 +10,7 @@ export default function Notifications() {
         <div className="mt-8 space-y-4">
             
 
-            <div className="flex items-center w-full px-5 py-2 transition-colors duration-200 dark:hover:bg-gray-800 gap-x-2 hover:bg-gray-100 focus:outline-none">
+            <div className="flex items-center w-full  px-5 py-2 border-b transition-colors duration-200 dark:hover:bg-gray-800 gap-x-2 hover:bg-gray-100 focus:outline-none">
                 <div className="text-center">
                     <h1 className="text-sm font-medium text-gray-700 capitalize dark:text-white">Mia send you a message!!</h1>    
                 </div>
