f729842eda48f4785ae8a6a4d01323ba020204ab
mohammed-rharbi <rharbi383@gmail.com>
2024-11-01 20:15:43 +0100

    feat : create the user create method to register new user

--- js-communication/src/users/users.controller.ts
+++ js-communication/src/users/users.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller , Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from './users.entity';
+import { CreateUserDTO } from './DTO/create-user.dto';
+
+
+
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    constructor(private readonly usersService: UsersService) {}
+
+    @Post('register')
+    async register(@Body() createUser : CreateUserDTO ){
+
+        return this.usersService.Create(createUser);
+    }
+
+
+
+
+
+
+}
