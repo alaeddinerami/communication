2681b5af9e11a681d2c32bbbb5503293f5416eac
mohammed-rharbi <rharbi383@gmail.com>
2024-11-09 21:40:49 +0100

    feat : add sweet alert

--- frontend/src/components/ui/chatHeader.tsx
+++ frontend/src/components/ui/chatHeader.tsx
@@ -1,4 +1,23 @@
+import Swal from 'sweetalert2';
+
+
 export default function ChatHeader() {
+
+
+    const handleiT = () => {
+        Swal.fire({
+          title: "You Want To Start a Call ?",
+          icon: "question",
+          confirmButtonText: "YES",
+          cancelButtonText: "NO",
+          showCancelButton: true,
+          showCloseButton: true,
+          customClass: {
+            popup: 'custom-dark-popup', 
+          }
+        });
+      };
+      
     return (
       <div className="w-full mx-auto">
         <div className="flex items-center justify-between py-2 px-3 bg-gray-50 rounded-lg dark:bg-gray-700">
@@ -10,13 +29,13 @@ export default function ChatHeader() {
           
 
           <div className="flex space-x-2">
-            <button type="button" className="p-2 text-gray-500 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
+            <button onClick={handleiT} type="button" className="p-2 text-gray-500 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
               <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
                 <path d="M798-120q-125 0-247-54.5T329-329Q229-429 174.5-551T120-798q0-18 12-30t30-12h162q14 0 25 9.5t13 22.5l26 140q2 16-1 27t-11 19l-97 98q20 37 47.5 71.5T387-386q31 31 65 57.5t72 48.5l94-94q9-9 23.5-13.5T670-390l138 28q14 4 23 14.5t9 23.5v162q0 18-12 30t-30 12ZM241-600l66-66-17-94h-89q5 41 14 81t26 79Zm358 358q39 17 79.5 27t81.5 13v-88l-94-19-67 67ZM241-600Zm358 358Z" />
               </svg>
             </button>
             
-            <button type="button" className="p-2 text-gray-500 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
+            <button onClick={handleiT} type="button" className="p-2 text-gray-500 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
               <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
                 <path d="M360-320h80v-120h120v-80H440v-120h-80v120H240v80h120v120ZM160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h480q33 0 56.5 23.5T720-720v180l160-160v440L720-420v180q0 33-23.5 56.5T640-160H160Zm0-80h480v-480H160v480Zm0 0v-480 480Z" />
               </svg>
