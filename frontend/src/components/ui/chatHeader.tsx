0fe20089a3d7c7428c4cb4b652b73aaae7669f0b
mohammed-rharbi <rharbi383@gmail.com>
2024-11-11 09:20:17 +0100

    perf: modify chat header to be clodes

--- frontend/src/components/ui/chatHeader.tsx
+++ frontend/src/components/ui/chatHeader.tsx
@@ -8,6 +8,11 @@ interface ChatHeaderProps {
 
 export default function ChatHeader({onProfileClick}: ChatHeaderProps ) {
 
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(prevState => !prevState);
+  };
 
     const handleiT = () => {
         Swal.fire({
@@ -22,6 +27,7 @@ export default function ChatHeader({onProfileClick}: ChatHeaderProps ) {
           }
         });
       };
+      
       
     return (
       <div className="w-full mx-auto">
@@ -52,14 +58,30 @@ export default function ChatHeader({onProfileClick}: ChatHeaderProps ) {
               </svg>
             </button>
             
-            <button  type="button" className="p-2 text-gray-500 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
+            <button onClick={toggleDropdown}  type="button" className="p-2 text-gray-500 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
               <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
                 <path d="M240-400q-33 0-56.5-23.5T160-480q0-33 23.5-56.5T240-560q33 0 56.5 23.5T320-480q0 33-23.5 56.5T240-400Zm240 0q-33 0-56.5-23.5T400-480q0-33 23.5-56.5T480-560q33 0 56.5 23.5T560-480q0 33-23.5 56.5T480-400Zm240 0q-33 0-56.5-23.5T640-480q0-33 23.5-56.5T720-560q33 0 56.5 23.5T800-480q0 33-23.5 56.5T720-400Z" />
               </svg>
             </button>
 
+            {isDropdownOpen && (
+
+            <div className="absolute  mt-14 w-48 bg-gray-700 text-gray-300 rounded-lg shadow-lg z-50">
+              <span className='flex justify-end p-2 '><svg className='cursor-pointer' onClick={toggleDropdown} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg></span>
+
+              <ul className="px-2 pb-2">
+
+                <li className=" flex gap-20 hover:bg-gray-600 p-2 rounded cursor-pointer">Settings<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="20px" fill="#e8eaed"><path d="m370-80-16-128q-13-5-24.5-12T307-235l-119 50L78-375l103-78q-1-7-1-13.5v-27q0-6.5 1-13.5L78-585l110-190 119 50q11-8 23-15t24-12l16-128h220l16 128q13 5 24.5 12t22.5 15l119-50 110 190-103 78q1 7 1 13.5v27q0 6.5-2 13.5l103 78-110 190-118-50q-11 8-23 15t-24 12L590-80H370Zm70-80h79l14-106q31-8 57.5-23.5T639-327l99 41 39-68-86-65q5-14 7-29.5t2-31.5q0-16-2-31.5t-7-29.5l86-65-39-68-99 42q-22-23-48.5-38.5T533-694l-13-106h-79l-14 106q-31 8-57.5 23.5T321-633l-99-41-39 68 86 64q-5 15-7 30t-2 32q0 16 2 31t7 30l-86 65 39 68 99-42q22 23 48.5 38.5T427-266l13 106Zm42-180q58 0 99-41t41-99q0-58-41-99t-99-41q-59 0-99.5 41T342-480q0 58 40.5 99t99.5 41Zm-2-140Z"/></svg></li>
+
+                <li className="flex gap-24 hover:bg-gray-600 p-2 rounded cursor-pointer">Muted <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M480-240q100 0 170-70t70-170q0-100-70-170t-170-70q-100 0-170 70t-70 170q0 100 70 170t170 70ZM360-440v-80h240v80H360ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg></li>
+
+                <li className="flex gap-24 hover:bg-gray-600 p-2 rounded cursor-pointer">Delete <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="red"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg> </li>
+              </ul>
+            </div>
+          )}
+
           </div>
-        </div>
+        </div>  
       </div>
     );
   }
