86cd89587c95c3d1cca0cb2fa41e8e8e639d5bb6
mohammed-rharbi <rharbi383@gmail.com>
2024-11-15 10:28:21 +0100

    feat : register friendRequest services and cotroller to module

--- js-communication/src/friends-request/friends-request/friends-request.module.ts
+++ js-communication/src/friends-request/friends-request/friends-request.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
 import { FriendsRequestController } from './friends-request.controller';
+import { FriendRequesRepositorie } from './friends-request.repostorie';
+import { FriendsRequestService } from './friends-request.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { FriendRequestSchema , FriendRequest } from './friends-request.entity'
+import { userSchema } from 'src/users/users.entity';
+import { User } from 'src/users/users.entity';
 
 @Module({
-  controllers: [FriendsRequestController]
+  controllers: [FriendsRequestController],
+  imports: [ MongooseModule.forFeature([{name: FriendRequest.name , schema: FriendRequestSchema} , 
+                                        {name: User.name , schema: userSchema}
+  ]) 
+                                        
+        ],
+  providers: [FriendRequesRepositorie , FriendsRequestService ]
 })
 export class FriendsRequestModule {}
