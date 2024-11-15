59c343d27bdb269df2181fadc78bd247f2f9fb2e
mohammed-rharbi <rharbi383@gmail.com>
2024-11-15 10:27:08 +0100

    feat : modify friendRequest controller

--- js-communication/src/friends-request/friends-request/friends-request.controller.ts
+++ js-communication/src/friends-request/friends-request/friends-request.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, NotFoundException, Param, Post } from '@nestjs/common';
+import { FriendsRequestService } from './friends-request.service';
 
-@Controller('friends-request')
-export class FriendsRequestController {}
+@Controller('friends/request')
+export class FriendsRequestController {
+  constructor(private readonly friendRequestService: FriendsRequestService) {}
+
+  
+  @Post(':fromId/send/:toId')
+  async sendFriendRequest(
+    @Param('fromId') fromId: string,
+    @Param('toId') toId: string ): Promise<any> {
+    try {
+      return await this.friendRequestService.sendFriendRequest(fromId, toId);
+    } catch (err) {
+      throw new NotFoundException('Error sending friend request: ' + err.message);
+    }
+  }
+
+  @Post(':requestId/approve')
+  async approveRequest(@Param('requestId') requestId: string): Promise<void> {
+    try {
+      await this.friendRequestService.approveRequest(requestId);
+    } catch (err) {
+      throw new NotFoundException('Error approving friend request: ' + err.message);
+    }
+  }
+}
