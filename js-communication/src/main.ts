c080b790f413577e79213ce45ad04df596d994cc
mohammed-rharbi <rharbi383@gmail.com>
2024-11-15 10:26:04 +0100

    feat : modify main.ts to include cors

--- js-communication/src/main.ts
+++ js-communication/src/main.ts
@@ -12,9 +12,9 @@ async function bootstrap() {
 
   app.enableCors({
     origin: ['http://localhost:5173'],
-    methods: 'GET,POST,PUT,DELETE', 
-    allowedHeaders: 'Content-Type, Accept',
-    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE','OPTIONS'], 
+    allowedHeaders: ['Content-Type','Accept','Authorization'],
+    // credentials: true,
   });
 
   await app.listen(process.env.PORT ?? 3000);
